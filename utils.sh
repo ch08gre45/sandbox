0f1952203818148a0f818a623d4f2a18f424c28c
ch08gre45 <drazvan85@gmail.com>
2016-05-25 03:21:23 +0300

    utils.sh
    Bash script utility library

---
+++ utils.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Utility scripts library
+
+setup_color() {
+  # Setup color variables
+  __gray="\033[1;30m"
+  __light_grey="\033[0;37m"
+  __cyan="\033[0;36m"
+  __light_cyan="\033[1;36m"
+  __no_color="\033[0m"
+  __black="\033[0;30m"
+  __blue="\033[0;34m"
+  __green="\033[0;32m"
+  __red="\033[0;31m"
+  __purple="\033[0;35m"
+  __brown="\033[0;33m"
+  __yellow="\033[1;33m"
+  __light_blue="\033[1;34m"
+  __light_green="\033[1;32m"
+  __light_red="\033[1;31m"
+  __light_purple="\033[1;35m"
+  __white="\033[1;37m"
+}
