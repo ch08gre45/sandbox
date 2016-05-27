baf568b1cfa5a6fd9812ca901b4565b9d25aa984
ch08gre45 <ch08gre45@users.noreply.github.com>
2016-05-27 12:30:12 +0300

    Added more terminal utility functions

--- screen_utils.sh
+++ screen_utils.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Utility scripts library
+# Utility scripts library for working with terminal display
 
+# Setup color variables
+# Usage echo -e "Normal text ${__color}Text of chosen color${__no_color} Normal text"
 setup_color() {
-  # Setup color variables
   __gray="\033[1;30m"
   __light_grey="\033[0;37m"
   __cyan="\033[0;36m"
@@ -22,3 +23,17 @@ setup_color() {
   __light_purple="\033[1;35m"
   __white="\033[1;37m"
 }
+
+# Snippet taken from http://top-scripts.blogspot.ro/2011/01/blog-post.html
+e='echo -en'                                     # shortened echo command variable
+   ESC=$( $e "\033")                             # variable containing escaped value
+ clear(){ $e "\033c";}                           # clear screen
+ civis(){ $e "\033[?25l";}                       # hide cursor
+ cnorm(){ $e "\033[?12l\033[?25h";}              # show cursor
+  tput(){ $e "\033[${1};${2}H";}                 # terminal put (x and y position)
+colput(){ $e "\033[${1}G";}                      # put text in the same line as the specified column
+  mark(){ $e "\033[7m";}                         # select current line text
+unmark(){ $e "\033[27m";}                        # normalize current line text
+  draw(){ $e "\033%@";echo -en "\033(0";}        # switch to 'garbage' mode to be able to draw
+ write(){ $e "\033(B";}                          # return to normal (reset)
+  blue(){ $e "\033c\033[0;1m\033[37;44m\033[J";} # clear screen, set background to blue and font to white
